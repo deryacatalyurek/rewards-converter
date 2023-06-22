153ca4ea62c8af55d7abeddd0985cbf8bc67196e
Derya C <deryacatalyurek@gmail.com>
2023-06-22 17:18:50 +0300

    first try rewardValue

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class RewardValue{
+    private double cashValue;
+    private double miles;
+
+    //Constructor 1 
+    public RewardValue(double cashValue){
+        this.cashValue = cashValue;
+        this.miles = cashValue / 0.0035;
+    }
+    //Constructor accepting miles value 
+    public RewardValue(double miles){
+        this.miles = miles;
+        miles = cashValue / 0.0035;
+        return miles;
+    }
+    public double getCashValue(){
+        return cashValue;
+    }
+    public double getMilesValue(){
+        return miles;
+    }
+
+}
